efc8e817a6155d086c973b86f39c64ef7cdec030
triple-w <44819650+triple-w@users.noreply.github.com>
2025-09-01 23:54:19 -0600

    Update FacturaUiController.php

--- app/Http/Controllers/Facturacion/FacturaUiController.php
+++ app/Http/Controllers/Facturacion/FacturaUiController.php
@@ -8,6 +8,8 @@ use App\Models\SeriesFolio;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 
 class FacturaUiController extends Controller
