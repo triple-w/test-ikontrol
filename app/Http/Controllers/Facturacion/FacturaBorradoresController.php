f936f1c81983cd9608bedcf3a19ec28b23cbf40f
Carlos Camarena <calcamaren@gmail.com>
2026-01-08 10:48:47 -0600

    Revert "Update FacturaBorradoresController.php"
    This reverts commit c57d42d9d8e1b0a8639e0d25828d241726d90557.

--- app/Http/Controllers/Facturacion/FacturaBorradoresController.php
+++ app/Http/Controllers/Facturacion/FacturaBorradoresController.php
@@ -36,12 +36,6 @@ class FacturaBorradoresController extends Controller
                         ->with('ok', 'Borrador #'.$borrador->id.' cargado en creación.');
     }
 
-    public function loadIntoCreate(FacturaBorrador $borrador)
-    {
-        // Alias para compatibilidad con la ruta/vista que llama a loadIntoCreate
-        return $this->openInCreate($borrador);
-    }
-
     public function destroy(FacturaBorrador $borrador)
     {
         // abort_unless($borrador->user_id === auth()->id(), 403);
